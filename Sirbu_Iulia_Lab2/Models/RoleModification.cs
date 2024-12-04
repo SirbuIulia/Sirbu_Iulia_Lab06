7cf2242984a1a741e07b71461ef03e90dbe0fe60
IULIA SÎRBU <sirbuiulia05@gmail.com>
2024-12-04 17:43:47 +0200

    Modification

--- Sirbu_Iulia_Lab2/Models/RoleModification.cs
+++ Sirbu_Iulia_Lab2/Models/RoleModification.cs
@@ -11,8 +11,8 @@ namespace Sirbu_Iulia_Lab2.Models
         [Required]
         public string RoleName { get; set; }
         public string RoleId { get; set; }
-        public string[] AddIds { get; set; }
-        public string[] DeleteIds { get; set; }
+        public string[]? AddIds { get; set; }
+        public string[]? DeleteIds { get; set; }
     }
 }
 
